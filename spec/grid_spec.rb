b4f58d3a7c01ed4aa235b17e99fd685468b94311
Eva Zamudio <gabriela.osu@gmail.com>
2014-01-07 18:43:06 -0500

    add grid spec for exceptions

--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,5 +2,17 @@ require 'rspec'
 require '../lib/grid.rb'
 
 describe Grid do
-  
+  # need to check for exceptions (grid empty , grid already taken)
+  describe "#available?" do
+    let(:grid){Grid.new(6,6)}
+
+    it "return true if the grid is empty" do
+      grid.available?(3,3).should eq(true)
+    end
+
+    it "raise an error if the grid is already taken" do
+      grid.grid[3][3] = "N"
+      expect{grid.available?(3,3)}.to raise_error
+    end
+  end
 end
